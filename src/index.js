069b7e316c0cc67b4554dcadc71a12f5bcfb0953
Timothy Beler <timothybeler4@gmail.com>
2023-01-06 03:44:43 -0500

    Got to step 4 in the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function component() {
-  const element = document.createElement('div');
+	const element = document.createElement("div");
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	// Lodash, currently included via a script, is required for this line to work
+	element.innerHTML = "Hello World";
 
-  return element;
+	return element;
 }
 
 document.body.appendChild(component());
