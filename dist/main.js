d39c56b47e272bfd5d77a619633db3cd04e6abc3
Timothy Beler <timothybeler4@gmail.com>
2023-01-07 04:41:58 -0500

    Moved the elements from index.html to index.js. Still debugging why the image isn't showing in the DOM.

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("function component() {\n\tconst element = document.createElement(\"div\");\n\n\t// Lodash, currently included via a script, is required for this line to work\n\telement.innerHTML = \"Hello World\";\n\n\treturn element;\n}\n\ndocument.body.appendChild(component());\n\n\n//# sourceURL=webpack://odin-project-1.3-restaurant-page/./src/index.js?");
+eval("function component() {\n\tconst content = document.getElementById(\"content\");\n\n\t// const element = document.createElement(\"div\");\n\tconst restaurantHeader = document.createElement(\"h1\");\n\tconst image = document.createElement(\"img\");\n\tconst about_wrapper = document.createElement(\"div\");\n\tconst about_content = document.createElement(\"p\");\n\n\t// Classlists\n\trestaurantHeader.classList.add(\"primary_clr\", \"nav_bar\");\n\tabout_content.classList.add(\"about_content\");\n\tabout_wrapper.classList.add(\"about_wrapper\", \"secondary_clr\");\n\n\timage.src = \"restaurant-img.jpg\";\n\timage.alt = \"A picture of a busy restaurant\";\n\tabout_content.innerHTML =\n\t\t\"Our restaurant is the best in town! We offer a wide variety of delicious dishes, a welcoming atmosphere, and friendly service. Come visit us and experience the best dining experience around.\";\n\n\tabout_wrapper.append(about_content);\n\n\tcontent.append(restaurantHeader, image, about_wrapper);\n\n\t// Lodash, currently included via a script, is required for this line to work\n\t// element.innerHTML = \"Hello World\";\n\n\treturn content;\n}\n\ndocument.body.append(component());\n\n\n//# sourceURL=webpack://odin-project-1.3-restaurant-page/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 function component() {
-	const element = document.createElement("div");
+	const content = document.getElementById("content");
+
+	// const element = document.createElement("div");
+	const restaurantHeader = document.createElement("h1");
+	const image = document.createElement("img");
+	const about_wrapper = document.createElement("div");
+	const about_content = document.createElement("p");
+
+	// Classlists
+	restaurantHeader.classList.add("primary_clr", "nav_bar");
+	about_content.classList.add("about_content");
+	about_wrapper.classList.add("about_wrapper", "secondary_clr");
+
+	image.src = "restaurant-img.jpg";
+	image.alt = "A picture of a busy restaurant";
+	about_content.innerHTML =
+		"Our restaurant is the best in town! We offer a wide variety of delicious dishes, a welcoming atmosphere, and friendly service. Come visit us and experience the best dining experience around.";
+
+	about_wrapper.append(about_content);
+
+	content.append(restaurantHeader, image, about_wrapper);
 
 	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = "Hello World";
+	// element.innerHTML = "Hello World";
 
-	return element;
+	return content;
 }
 
-document.body.appendChild(component());
+document.body.append(component());
